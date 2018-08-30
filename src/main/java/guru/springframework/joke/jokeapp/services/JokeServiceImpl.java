dc85499956482c88ffa7c69625643517d2aa6e49
Jens Wambach <jens.wambach@t-online.de>
2018-08-30 16:36:57 +0200

    safety commit

--- src/main/java/guru/springframework/joke/jokeapp/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/jokeapp/services/JokeServiceImpl.java
@@ -17,6 +17,6 @@ public class JokeServiceImpl implements JokeService {
 
     @Override
     public String getJoke() {
-        return null;
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
